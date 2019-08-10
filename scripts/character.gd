71bcc47ba42564c2faf8eb133dcc322bfacd2b68
Christian Lowe <christianlowe.cl@gmail.com>
2019-08-10 12:33:14 -0700

    Add base framework for impulse functionality

--- scripts/character.gd
+++ scripts/character.gd
@@ -5,15 +5,19 @@ export(String, FILE, '*tscn') var plant_scene_path
 export var movement_speed = 50
 var velocity = Vector2(0,0)
 
+var impulse = Vector2.ZERO
+var scaled = 100.0
+export var dampening = 3.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity.y = 0
 	velocity.x = 0
-		
+	velocity.y = 0
+	
 	if Input.is_action_pressed('left'):
 		velocity.x = -movement_speed
 		$KinematicBody2D/Sprite.flip_h = true
@@ -31,13 +35,21 @@ func _physics_process(delta):
 	#else:
 	#	$KinematicBody2D/Sprite.animation = 'idle'
 	
-	$KinematicBody2D.move_and_slide(velocity*delta)
+	var movement = velocity * delta
+	
+	if scaled > 0:
+		scaled -= dampening
+		movement += impulse * (scaled / 100)
+	
+	$KinematicBody2D.move_and_slide(movement)
 
 func _input(event):
 	if event.is_action_released('shoot'):
-		var projectile = load(projectile_scene_path).instance()
-		add_child(projectile)
-		projectile.global_position = $KinematicBody2D.global_position
+		impulse = Vector2(200, 500)
+		scaled = 100
+#		var projectile = load(projectile_scene_path).instance()
+#		add_child(projectile)
+#		projectile.global_position = $KinematicBody2D.global_position
 	if event.is_action_released('plant'):
 		var plant = load(plant_scene_path).instance()
 		add_child(plant)
