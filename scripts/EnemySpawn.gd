0422973b5e8d256d40fa3a60c671503b02c02657
David Dalisay <dalisay.david3@gmail.com>
2019-08-16 16:11:44 -0400

    Added UFO.

--- scripts/EnemySpawn.gd
+++ scripts/EnemySpawn.gd
@@ -5,13 +5,25 @@ export(String, FILE, '*tscn') var enemy_scene_path
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.wait_time = randi()%45+30
-	$Timer.connect('timeout', self, 'spawn_enemy')
-
+	$Timer.connect('timeout', self, 'send_ufo_beam')
+	$AnimationPlayer.play('Float')
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func send_ufo_beam():
+	$EnemySpawnLaser/AnimationPlayer.play('Expose')
+
+func bring_back_ufo_beam():
+	$EnemySpawnLaser/AnimationPlayer.play('Hide')
 
 func spawn_enemy():
 	var enemy = load(enemy_scene_path).instance()
 	get_parent().add_child(enemy)
 	enemy.global_position = self.global_position
+	bring_back_ufo_beam()
+	
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == 'Expose':
+		spawn_enemy()
---
+++ scripts/EnemySpawnLaser.gd
@@ -0,0 +1,38 @@
+extends Line2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var hidden = Color(.33,.58,.81,0)
+var exposed = Color(.33,.58,.81,.81)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	$HideEffect.interpolate_property(
+#		self,
+#		'default_color',
+#		exposed,
+#		hidden,
+#		0.75,
+#		Tween.TRANS_CUBIC,
+#		Tween.EASE_OUT 
+#	)
+#
+#	$ExposeEffect.interpolate_property(
+#		self,
+#		'default_color',
+#		hidden,
+#		exposed,
+#		0.75,
+#		Tween.TRANS_CUBIC,
+#		Tween.EASE_OUT 
+#	)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
