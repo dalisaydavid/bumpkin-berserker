b5b0d467c43b84bb1d0677db4f63dd8a579e9d47
Tyler Hamm <tyhamm10@gmail.com>
2019-08-16 16:24:03 -0700

    minimap fix for res. background increase.

--- scripts/Minimap.gd
+++ scripts/Minimap.gd
@@ -26,13 +26,13 @@ func _ready():
 	var width = scene_width / ratio
 	var height = scene_height / ratio
 	
+	#width = $Camera2D.get_
+	
 	var root_node = get_parent().get_parent()
 	character = root_node.get_node("Character").get_node("KinematicBody2D")
 	plots = root_node.get_node("PlantingAreas").get_children()
 	
-	print(width)
-	
-	origin = Vector2(scene_width, offset)
+	origin = Vector2(scene_width-(offset*6), offset)
 	scene_size = Vector2(scene_width, scene_height)
 	map_size = Vector2(width, height)
 	map_rect = Rect2(origin, map_size)
